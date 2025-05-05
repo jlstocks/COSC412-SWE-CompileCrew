af895a1dc2d99dd7e1f8238a5ffcdd7cae3f74a5
jstocks1 <jstocks1@students.towson.edu>
2025-05-05 14:44:36 -0400

    updated schema syntax

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,8 @@ CREATE TABLE User (
     userID INT AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,
-    PRIMARY KEY userID
+    _password VARCHAR(100) NOT NULL,
+    PRIMARY KEY (userID)
 );
 
 # Create Account table
@@ -12,16 +12,16 @@ CREATE TABLE Account (
     accID INT AUTO_INCREMENT,
     userID INT NOT NULL,
     balance DECIMAL(10,2) DEFAULT 0.00,
-    PRIMARY KEY accID,
+    PRIMARY KEY (accID),
     FOREIGN KEY (userID) REFERENCES User(userID) ON DELETE CASCADE
 );
 
 # Create Category table
 CREATE TABLE Category (
     categoryID INT AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    description TEXT
-    PRIMARY KEY categoryID
+    _name VARCHAR(50) NOT NULL,
+    _description TEXT,
+    PRIMARY KEY (categoryID)
 );
 
 # Create Budget table
@@ -31,7 +31,7 @@ CREATE TABLE Budget (
     limitAmount DECIMAL(10,2) NOT NULL,
     startDate DATE NOT NULL,
     endDate DATE NOT NULL,
-    PRIMARY KEY budgetID
+    PRIMARY KEY (budgetID),
     FOREIGN KEY (userID) REFERENCES User(userID) ON DELETE CASCADE
 );
 
@@ -42,8 +42,8 @@ CREATE TABLE Expenses (
     accID INT NOT NULL,
     categoryID INT,
     amount DECIMAL(10,2) NOT NULL,
-    date DATE NOT NULL,
-    PRIMARY KEY expenseID,
+    _date DATE NOT NULL,
+    PRIMARY KEY (expenseID),
     FOREIGN KEY (userID) REFERENCES User(userID) ON DELETE CASCADE,
     FOREIGN KEY (accID) REFERENCES Account(accID) ON DELETE CASCADE,
     FOREIGN KEY (categoryID) REFERENCES Category(categoryID) ON DELETE SET NULL
@@ -55,7 +55,7 @@ CREATE TABLE Report (
     userID INT NOT NULL,
     dateGenerated DATE NOT NULL,
     summary TEXT,
-    PRIMARY KEY reportID,
+    PRIMARY KEY (reportID),
     FOREIGN KEY (userID) REFERENCES User(userID) ON DELETE CASCADE
 );
 
@@ -64,7 +64,7 @@ CREATE TABLE SharedReport (
     sharedID INT AUTO_INCREMENT,
     reportID INT NOT NULL,
     sharedWithUserID INT NOT NULL,
-    PRIMARY KEY sharedID
+    PRIMARY KEY (sharedID),
     FOREIGN KEY (reportID) REFERENCES Report(reportID) ON DELETE CASCADE,
     FOREIGN KEY (sharedWithUserID) REFERENCES User(userID) ON DELETE CASCADE
 );
