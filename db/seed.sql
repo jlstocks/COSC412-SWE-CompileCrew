e212c90f8cefc8715ceb998b799b082f499f5b9f
Jackson Stockstill <jacksonstockstill@proton.me>
2025-05-06 01:37:30 -0400

    Fixed seed.sql syntax

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,6 @@
 # Insert Sample Data into Tables for Testing and Demonstration
 # User Table Data
-INSERT INTO User (userID, username, email, password) VALUES 
+INSERT INTO User (userID, username, email, _password) VALUES 
 (1, 'jstocks1', 'jstocks1@students.towson.edu', 'password1!');
 
 # Account Table Data
@@ -8,7 +8,7 @@ INSERT INTO Account (accID, userID, balance) VALUES
 (1, 1, 1400.75);
 
 # Category Table Data
-INSERT INTO Category (categoryID, name, description) VALUES 
+INSERT INTO Category (categoryID, _name, _description) VALUES 
 (1, 'Food', 'Expenses for groceries and restaurants.'),
 (2, 'Transportation', 'Expenses for gas, bus, train, etc.'),
 (3, 'Recreation', 'Expenses for recreational activity.'),
@@ -21,7 +21,7 @@ INSERT INTO Budget (budgetID, userID, limitAmount, startDate, endDate) VALUES
 (2, 1, 1500.00, '2025-05-01', ' 2025-05-31');
 
 # Expenses Table Data
-INSERT INTO Expenses (expenseID, userID, accID, categoryID, amount, date) VALUES 
+INSERT INTO Expenses (expenseID, userID, accID, categoryID, amount, _date) VALUES 
 (1, 1, 1, 1, 35.50, '2025-04-12'),
 (2, 1, 1, 2, 42.75, '2025-04-20');
 
